ba5c3f4b6189466c3f7938ec325f17752dd98dc2
Han Xiao <han.xiao@cs.helsinki.fi>
2015-10-28 21:28:59 +0200

    added/tested polytree construction

---
+++ poly_tree.py
@@ -0,0 +1,40 @@
+# construct the polytree
+
+def construct_ptree(labelset_list):
+    """
+    labelset_list: list of labelset in order
+
+    Return:
+    ---------
+    list of tuples: the edges(where node are the index of the labelset in labelset_list)
+    """
+    tree = [] # for the ith element, it stores the ith labelset's parent node ids
+    for i in xrange(len(labelset_list)):
+        tree.append([])
+
+    frontier_node_ids = set()
+    
+    for new_node_id, new_node in enumerate(labelset_list):
+        # iterate through the current frontier list
+        # and find all possible parent for the new_node
+        frontiers_to_be_removed = []
+        for f_node_id in frontier_node_ids:
+            node_id_queue = [f_node_id]
+            while len(node_id_queue) > 0:
+                cur_node_id = node_id_queue.pop(0)
+                if labelset_list[cur_node_id].intersection(new_node):
+                    tree[new_node_id].append(cur_node_id)
+                    if cur_node_id in frontier_node_ids:
+                        frontiers_to_be_removed.append(cur_node_id)
+                    break
+                else:
+                    for parent_id in tree[cur_node_id]:
+                        node_id_queue.append(parent_id)
+        frontier_node_ids -= set(frontiers_to_be_removed)
+        frontier_node_ids.add(new_node_id)
+    # edges = []
+    # for node_id, parent_ids in enumerate(tree):
+    #     for p_id in parent_ids:
+    #         edges.append((p_id, node_id))
+    # return edges
+    return tree
---
+++ test.py
@@ -0,0 +1,18 @@
+from nose.tools import assert_equal
+from poly_tree import construct_ptree
+
+def test_construct_ptree():
+    A, B, C, D = 'A', 'B', 'C', 'D'
+    labelset_list = [(A, B, C),
+                     (B),
+                     (A, C),
+                     tuple([D]),
+                     (B, C),
+                     (A)]
+    labelset_list = map(set, labelset_list)
+    actual_tree = construct_ptree(labelset_list)
+    expected_tree = [[], [0], [0], [], [1, 2], [2]]
+    # [(0, 1), (0, 2),
+    #                   (1, 4), (2, 4), (2, 5)]
+    assert_equal(actual_tree, expected_tree)
+    
